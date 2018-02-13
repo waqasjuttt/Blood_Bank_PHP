412e1557870bfc0dce4f681c02f46ce656187891
Waqas Jutt <waqas.jutt814@gmail.com>
2018-02-13 13:11:33 +0500

    3rd commit

---
+++ Blood_Bank/Blood_Bags.php
@@ -0,0 +1,39 @@
+<?php
+require_once 'DbOperations.php';
+ 
+$response = array(); 
+ 
+if($_SERVER['REQUEST_METHOD']=='POST'){
+    if(
+            isset($_POST['mobile']) and
+            isset($_POST['blood_group']) and
+            isset($_POST['blood_bottle']) and
+            isset($_POST['city']) and
+            isset($_POST['hospital']))
+        {
+        //operate the data further 
+ 
+        $db = new DbOperations();
+ 
+        $result = $db->EditUserRequest(
+            $_POST['mobile']
+            , $_POST['blood_group']
+            , $_POST['blood_bottle']
+            , $_POST['city']
+            , $_POST['hospital']);
+        if($result == 1){
+            $response['error'] = false;
+            $response['message'] = "Request update successfully";
+        }
+        else{       
+            $response['error'] = true; 
+            $response['message'] = "Request not updated";
+        }
+    }
+}else{
+    $response['error'] = true; 
+    $response['message'] = "Invalid Request";
+}
+ 
+echo json_encode($response);
+?>
