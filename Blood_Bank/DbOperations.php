00b86ee0a7a786ba9ab00b97a7224195718ca2be
Waqas Jutt <waqas.jutt814@gmail.com>
2018-02-13 13:11:48 +0500

    3rd commit

--- Blood_Bank/DbOperations.php
+++ Blood_Bank/DbOperations.php
@@ -20,6 +20,17 @@
             $password = md5($pass);
             $stmt = $this->con->prepare("INSERT INTO `blood` (`id`, `mobile`, `name`, `password`) VALUES (NULL, ?, ?, ?);");
             $stmt->bind_param("sss", $mobile_number, $name, $password);
+            $this->createUserRequest($mobile_number, $name);
+            if($stmt->execute()){
+                return 1; 
+            }else{
+                return 2; 
+            }
+        }
+        
+        private function createUserRequest($mobile_number, $name){
+            $stmt = $this->con->prepare("INSERT INTO `blood_request` (`id`, `mobile`, `name`) VALUES (NULL, ?, ?);");
+            $stmt->bind_param("ss", $mobile_number, $name);
             if($stmt->execute()){
                 return 1; 
             }else{
@@ -101,6 +112,29 @@
         {
             $stmt = $this->con->prepare("UPDATE blood SET name = ?, blood_group = ?, city = ?, address = ? , dob = ? WHERE  mobile = ?");
             $stmt->bind_param("ssssss", $name, $blood_group, $city, $address, $dob, $mobile_number);
+            $this->EditUserBloodRequest($name, $mobile_number);
+            if($stmt->execute()){
+                return 1;
+            }else{
+                return 2;
+            }
+        }
+        
+        private function EditUserBloodRequest($name, $mobile_number)
+        {
+            $stmt = $this->con->prepare("UPDATE blood_request SET name = ? WHERE  mobile = ?");
+            $stmt->bind_param("ss", $name, $mobile_number);
+            if($stmt->execute()){
+                return 1;
+            }else{
+                return 2;
+            }
+        }
+        
+        public function EditUserRequest($mobile_number, $blood_group, $blood_bottle, $city, $hospital)
+        {
+            $stmt = $this->con->prepare("UPDATE blood_request SET blood_group = ?, blood_bottle = ?, city = ? , hospital = ? WHERE  mobile = ?");
+            $stmt->bind_param("sssss", $blood_group, $blood_bottle, $city, $hospital, $mobile_number);
             if($stmt->execute()){
                 return 1;
             }else{
